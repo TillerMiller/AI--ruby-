7554f7ae6f066ab024b3778d49f411d0fd684f0c
Tyler Miller <tyler.shawn.miller@gmail.com>
2014-02-21 10:23:22 -0800

    Update Browsie.rb
    Added more randomized search options

--- BrowseWeb/Browsie.rb
+++ BrowseWeb/Browsie.rb
@@ -1,15 +1,28 @@
 #!/usr/bin/env ruby
 
 require 'selenium-webdriver'
-require 'active_support'
 require 'faker'
 require 'yaml'
+require 'i_heart_quotes'
 
 def Search()
 	$driver.navigate.to "http://bing.com"
 	sleep 5
 	searchBar = $driver.find_element(:name, 'q')
-	searchBar.send_keys Faker::Company.catch_phrase()
+	case Random.rand(1...4)
+		when 1 then
+			searchBar.send_keys Faker::Company.catch_phrase()
+		when 2 then
+			searchBar.send_keys Faker::Name.name()
+		when 3 then 
+			case Random.rand(1...3)
+				when 1 then searchBar.send_keys Faker::Base.state() 
+				when 2 then searchBar.send_keys Faker::Base.city()
+				when 3 then searchBar.send_keys Faker::Base.country()
+			end
+		when 4 then
+			searchBar.send_keys IHeartQuotes::Client.random
+	end
 	searchBar.submit
 	sleep 5 
 end
